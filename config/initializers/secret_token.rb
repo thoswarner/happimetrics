84cf414743b005ab4ec729d75692869ae28e1a95
Tom Warner <thoswarner@gmail.com>
2012-12-03 20:46:04 +0000

    Adding base rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Happimetrics::Application.config.secret_token = '1babe00a522fbe300bf143c35e660fb290ded87c3baefce2a59e2e867dd7d2b5db608a59faabeddc917cc998b961c98130b418c3f89638c6906ee6665685c4bf'
