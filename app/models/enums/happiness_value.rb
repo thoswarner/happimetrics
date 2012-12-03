3442c8cf15c8d8a9da705f0f89cf2b832acde40a
Tom Warner <thoswarner@gmail.com>
2012-12-03 23:10:29 +0000

    adding happiness entry fauxification and tableless model

---
+++ app/models/enums/happiness_value.rb
@@ -0,0 +1,5 @@
+class HappinessValue < ActiveEnum::Base
+  value :id => 1, :name => :happy
+  value :id => 2, :name => :ok
+  value :id => 3, :name => :sad
+end
---
+++ app/models/happiness_entry.rb
@@ -0,0 +1,70 @@
+class HappinessEntry
+
+  # attributes
+  attr_accessor :entered_at, :value
+
+  # make this class look like a model
+  include ActiveModel::Validations
+  include ActiveModel::Conversion
+  include ActiveEnum::Extensions
+  extend ActiveModel::Naming
+
+  # enumerables
+  enumerate :happiness_value
+
+  def self.attr_accessor(*vars)
+    @attributes ||= []
+    @attributes.concat( vars )
+    super
+  end
+
+ def self.attributes
+   @attributes
+ end
+
+ def initialize(attributes={})
+   attributes && attributes.each do |name, value|
+     send("#{name}=", value) if respond_to? name.to_sym 
+   end
+ end
+
+  def persisted?
+    false
+  end
+
+  def self.inspect
+    "#<#{ self.to_s} #{ self.attributes.collect{ |e| ":#{ e }" }.join(', ') }>"
+  end
+
+  # create relatively random data for every day in the last 3 months
+  def self.fauxify!
+    # instantiate beginning date
+    date = 3.months.ago.to_date
+    # object to stash entries
+    happiness_entries = []
+    # loop through the days till today
+    until date == Date.today
+      # choose a random number of entries between 3 and 25
+      number_of_entries = (3..15).to_a.sample
+      # array to stash the current dates entries
+      happiness_entries_for_date = []
+      # create the given number of entries
+      number_of_entries.times do
+        hour = (9..17).to_a.sample
+        minute = (0..59).to_a.sample
+        # choose a random time in the day between 09:00 and 17:30 and a random happiness value
+        entered_at = DateTime.parse("#{date} #{hour}:#{minute}")
+        value = HappinessValue[[1,2,3].sample]
+        happiness_entries_for_date << self.new(:entered_at => entered_at, :value => value)
+      end
+      Rails.cache.write date, happiness_entries_for_date, :expires_in => 5.minutes
+      date = date + 1.day
+    end
+  end
+
+  def self.all_by_date date
+    entries = Rails.cache.fetch date
+    entries.sort! { |a,b| a.entered_at <=> b.entered_at }
+  end
+
+end
